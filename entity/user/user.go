01a71829e117754371a4695b614c3175ef32d07a
Alireza Bozorgomid <xbozorg@gmail.com>
2022-05-05 12:28:03 -0400

    Add user entity

---
+++ entity/user/user.go
@@ -0,0 +1,12 @@
+package user
+
+type User struct {
+	ID           string        `json:"id"` //UUID
+	Email        string        `json:"email"`
+	Password     string        `json:"password"`
+	Username     string        `json:"username"`
+	FirstName    string        `json:"firstName"`
+	LastName     string        `json:"lastName"`
+	PhoneNumbers []PhoneNumber `json:"phoneNumbers"`
+	Addresses    []Address     `json:"addresses"`
+}
