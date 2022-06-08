ab74ebeeafed4c94977eedc3ff7eef78f16be6a5
Alireza Bozorgomid <xbozorg@gmail.com>
2022-06-08 06:09:18 -0400

    Add mysql connection

---
+++ adapter/repository/mysql/connect.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/XBozorg/bookstore/config"
+	"github.com/go-sql-driver/mysql"
+)
+
+func Connect(conf *config.MySQLConfig) (MySQLRepo, error) {
+
+	cfg := mysql.Config{
+		User:   conf.User,
+		Passwd: conf.Pass,
+		Net:    conf.Net,
+		Addr:   conf.Address,
+		DBName: conf.Name,
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		return MySQLRepo{}, err
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		return MySQLRepo{}, err
+	}
+
+	var repo MySQLRepo
+	repo.SetDB(db)
+
+	return repo, nil
+}
