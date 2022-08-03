c06635eb07792a10b728b0436aed3293a27c8d93
Alireza Bozorgomid <xbozorg@gmail.com>
2022-08-03 15:32:07 -0400

    Add admin repository interfaces

---
+++ usecase/admin/admin_repository.go
@@ -0,0 +1,18 @@
+package admin
+
+import (
+	"context"
+
+	"github.com/XBozorg/bookstore/entity/admin"
+)
+
+type Repository interface {
+	GetAdmin(ctx context.Context, adminID string) (admin.Admin, error)
+	GetAdmins(ctx context.Context) ([]admin.Admin, error)
+	LoginAdmin(ctx context.Context, email, password string) (admin.Admin, error)
+	DoesAdminExist(ctx context.Context, adminID string) (bool, error)
+}
+
+type ValidatorRepo interface {
+	DoesAdminExist(ctx context.Context, adminID string) (bool, error)
+}
