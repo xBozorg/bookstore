1ef8855e65b75057affae28886ec04b67fe24da1
Alireza Bozorgomid <xbozorg@gmail.com>
2022-08-04 08:42:59 -0400

    Add book dto

---
+++ dto/book.go
@@ -0,0 +1,94 @@
+package dto
+
+type AddAuthorRequest struct{}
+type AddAuthorResponse struct{}
+
+type GetAuthorRequest struct{}
+type GetAuthorResponse struct{}
+
+type GetAuthorsRequest struct{}
+type GetAuthorsResponse struct{}
+
+type DeleteAuthorRequest struct{}
+type DeleteAuthorResponse struct{}
+
+type AddPublisherRequest struct{}
+type AddPublisherResponse struct{}
+
+type GetPublisherRequest struct{}
+type GetPublisherResponse struct{}
+
+type GetBookPublishersRequest struct{}
+type GetBookPublishersResponse struct{}
+
+type GetAllPublishersRequest struct{}
+type GetAllPublishersResponse struct{}
+
+type DeletePublisherRequest struct{}
+type DeletePublisherResponse struct{}
+
+type AddTopicRequest struct{}
+type AddTopicResponse struct{}
+
+type GetTopicRequest struct{}
+type GetTopicResponse struct{}
+
+type GetBookTopicsRequest struct{}
+type GetBookTopicsResponse struct{}
+
+type GetAllTopicsRequest struct{}
+type GetAllTopicsResponse struct{}
+
+type DeleteTopicRequest struct{}
+type DeleteTopicResponse struct{}
+
+type AddLanguageRequest struct{}
+type AddLanguageResponse struct{}
+
+type GetLanguageRequest struct{}
+type GetLanguageResponse struct{}
+
+type GetAllLanguagesRequest struct{}
+type GetAllLanguagesResponse struct{}
+
+type GetBookLanguagesRequest struct{}
+type GetBookLanguagesResponse struct{}
+
+type DeleteLanguageRequest struct{}
+type DeleteLanguageResponse struct{}
+
+type AddCoverRequest struct{}
+type AddCoverResponse struct{}
+
+type GetCoverRequest struct{}
+type GetCoverResponse struct{}
+
+type DeleteCoverRequest struct{}
+type DeleteCoverResponse struct{}
+
+type AddBookRequest struct{}
+type AddBookResponse struct{}
+
+type GetBookRequest struct{}
+type GetBookResponse struct{}
+
+type EditBookRequest struct{}
+type EditBookResponse struct{}
+
+type GetAllBooksRequest struct{}
+type GetAllBooksResponse struct{}
+
+type GetAuthorBooksRequest struct{}
+type GetAuthorBooksResponse struct{}
+
+type GetPublisherBooksRequest struct{}
+type GetPublisherBooksResponse struct{}
+
+type GetTopicBooksRequest struct{}
+type GetTopicBooksResponse struct{}
+
+type GetLangBooksRequest struct{}
+type GetLangBooksResponse struct{}
+
+type DeleteBookRequest struct{}
+type DeleteBookResponse struct{}
