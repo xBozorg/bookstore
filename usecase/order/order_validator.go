ddb363ab3fdbfab0053c7b02680c8f47a66b5ceb
Alireza Bozorgomid <xbozorg@gmail.com>
2022-08-13 09:25:04 -0400

    Add order validator types

---
+++ usecase/order/order_validator.go
@@ -0,0 +1,36 @@
+package order
+
+import (
+	"context"
+
+	"github.com/XBozorg/bookstore/dto"
+)
+
+type (
+	ValidateCreateEmptyOrder func(ctx context.Context, req dto.CreateEmptyOrderRequest) error
+	ValidateCheckOpenOrder   func(ctx context.Context, req dto.CheckOpenOrderRequest) error
+
+	ValidateAddItem          func(ctx context.Context, req dto.AddItemRequest) error
+	ValidateIncreaseQuantity func(ctx context.Context, req dto.IncreaseQuantityRequest) error
+	ValidateDecreaseQuantity func(ctx context.Context, req dto.DecreaseQuantityRequest) error
+	ValidateGetOrderItems    func(ctx context.Context, req dto.GetOrderItemsRequest) error
+	ValidateRemoveItem       func(ctx context.Context, req dto.RemoveItemRequest) error
+
+	ValidateCreatePromoCode func(ctx context.Context, req dto.CreatePromoCodeRequest) error
+	ValidateDeletePromoCode func(ctx context.Context, req dto.DeletePromoCodeRequest) error
+
+	ValidateSetOrderStatus   func(ctx context.Context, req dto.SetOrderStatusRequest) error
+	ValidateSetOrderSTN      func(ctx context.Context, req dto.SetOrderSTNRequest) error
+	ValidateSetOrderPromo    func(ctx context.Context, req dto.SetOrderPromoRequest) error
+	ValidateRemoveOrderPromo func(ctx context.Context, req dto.RemoveOrderPromoRequest) error
+	ValidateDeleteOrder      func(ctx context.Context, req dto.DeleteOrderRequest) error
+
+	ValidateGetAllOrdersByStatus  func(ctx context.Context, req dto.GetAllOrdersByStatusRequest) error
+	ValidateGetUserOrders         func(ctx context.Context, req dto.GetUserOrdersRequest) error
+	ValidateGetUserOrdersByStatus func(ctx context.Context, req dto.GetUserOrdersByStatusRequest) error
+	ValidateGetDateOrders         func(ctx context.Context, req dto.GetDateOrdersRequest) error
+	ValidateGetDateOrdersByStatus func(ctx context.Context, req dto.GetDateOrdersByStatusRequest) error
+
+	ValidateGetUserPromos   func(ctx context.Context, req dto.GetUserPromosRequest) error
+	ValidateGetPromoByOrder func(ctx context.Context, req dto.GetPromoByOrderRequest) error
+)
