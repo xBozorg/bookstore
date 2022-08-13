effa74c6c2ab835602664cc234d5ccdc7253a3b4
Alireza Bozorgomid <xbozorg@gmail.com>
2022-08-13 09:24:23 -0400

    Add order entity usecase interface and dto

---
+++ dto/order.go
@@ -0,0 +1,156 @@
+package dto
+
+import "github.com/XBozorg/bookstore/entity/order"
+
+type CreateEmptyOrderRequest struct {
+	UserID string `json:"userID"`
+}
+type CreateEmptyOrderResponse struct {
+	OrderID uint `json:"orderID"`
+}
+
+type CheckOpenOrderRequest struct {
+	UserID string `json:"userID"`
+}
+type CheckOpenOrderResponse struct {
+	OrderID uint `json:"orderID"`
+}
+
+type AddItemRequest struct {
+	UserID string     `json:"userID"`
+	Item   order.Item `json:"item"`
+}
+type AddItemResponse struct{}
+
+type IncreaseQuantityRequest struct {
+	OrderID uint `json:"orderID"`
+	ItemID  uint `json:"itemID"`
+}
+type IncreaseQuantityResponse struct{}
+
+type DecreaseQuantityRequest struct {
+	OrderID uint `json:"orderID"`
+	ItemID  uint `json:"itemID"`
+}
+type DecreaseQuantityResponse struct{}
+
+type GetOrderItemsRequest struct {
+	OrderID uint `json:"orderID"`
+}
+type GetOrderItemsResponse struct {
+	Items []order.Item `json:"items"`
+}
+
+type RemoveItemRequest struct {
+	OrderID uint `json:"orderID"`
+	ItemID  uint `json:"itemID"`
+}
+type RemoveItemResponse struct{}
+
+type CreatePromoCodeRequest struct {
+	UserID string      `json:"userID"`
+	Promo  order.Promo `json:"promo"`
+}
+type CreatePromoCodeResponse struct{}
+
+type DeletePromoCodeRequest struct {
+	PromoID uint `json:"promoID"`
+}
+type DeletePromoCodeResponse struct{}
+
+type SetOrderStatusRequest struct {
+	OrderID uint `json:"orderID"`
+	Status  uint `json:"status"`
+}
+type SetOrderStatusResponse struct{}
+
+type GetOrderStatusRequest struct {
+	OrderID uint `json:"orderID"`
+}
+type GetOrderStatusResponse struct {
+	Status uint `json:"status"`
+}
+
+type SetOrderSTNRequest struct {
+	OrderID uint   `json:"orderID"`
+	STN     string `json:"stn"`
+}
+type SetOrderSTNResponse struct{}
+
+type SetOrderPromoRequest struct {
+	UserID    string `json:"userID"`
+	OrderID   uint   `json:"orderID"`
+	PromoCode string `json:"promoCode"`
+}
+type SetOrderPromoResponse struct{}
+
+type RemoveOrderPromoRequest struct {
+	OrderID uint `json:"orderID"`
+}
+type RemoveOrderPromoResponse struct{}
+
+type DeleteOrderRequest struct {
+	OrderID uint `json:"orderID"`
+}
+type DeleteOrderResponse struct{}
+
+type GetAllOrdersRequest struct{}
+type GetAllOrdersResponse struct {
+	Orders []order.Order `json:"orders"`
+}
+
+type GetAllOrdersByStatusRequest struct {
+	Status uint `json:"status"`
+}
+type GetAllOrdersByStatusResponse struct {
+	Orders []order.Order `json:"orders"`
+}
+
+type GetUserOrdersRequest struct {
+	UserID string `json:"userID"`
+}
+type GetUserOrdersResponse struct {
+	Orders []order.Order `json:"orders"`
+}
+
+type GetUserOrdersByStatusRequest struct {
+	UserID string `json:"userID"`
+	Status uint   `json:"status"`
+}
+type GetUserOrdersByStatusResponse struct {
+	Orders []order.Order `json:"orders"`
+}
+
+type GetDateOrdersRequest struct {
+	Date string `json:"date"`
+}
+type GetDateOrdersResponse struct {
+	Orders []order.Order `json:"orders"`
+}
+
+type GetDateOrdersByStatusRequest struct {
+	Date   string `json:"date"`
+	Status uint   `json:"status"`
+}
+type GetDateOrdersByStatusResponse struct {
+	Orders []order.Order `json:"orders"`
+}
+
+type GetAllPromosRequest struct{}
+type GetAllPromosResponse struct {
+	Promos []order.Promo `json:"promos"`
+}
+
+type GetPromoByOrderRequest struct {
+	OrderID uint `json:"orderID"`
+}
+type GetPromoByOrderResponse struct {
+	Promo order.Promo `json:"promo"`
+}
+
+type GetUserPromosRequest struct {
+	UserID string `json:"userID"`
+}
+type GetUserPromosResponse struct {
+	Promos []order.Promo `json:"promos"`
+}
---
+++ usecase/order/order_usecase.go
@@ -0,0 +1,290 @@
+package order
+
+import (
+	"context"
+
+	"github.com/XBozorg/bookstore/dto"
+)
+
+type UseCase interface {
+	AddItem(ctx context.Context, req dto.AddItemRequest) (dto.AddItemResponse, error)
+	IncreaseQuantity(ctx context.Context, req dto.IncreaseQuantityRequest) (dto.IncreaseQuantityResponse, error)
+	DecreaseQuantity(ctx context.Context, req dto.DecreaseQuantityRequest) (dto.DecreaseQuantityResponse, error)
+	GetOrderItems(ctx context.Context, req dto.GetOrderItemsRequest) (dto.GetOrderItemsResponse, error)
+	RemoveItem(ctx context.Context, req dto.RemoveItemRequest) (dto.RemoveItemResponse, error)
+
+	CreatePromoCode(ctx context.Context, req dto.CreatePromoCodeRequest) (dto.CreatePromoCodeResponse, error)
+	DeletePromoCode(ctx context.Context, req dto.CreatePromoCodeRequest) (dto.CreatePromoCodeResponse, error)
+
+	SetOrderStatus(ctx context.Context, req dto.SetOrderStatusRequest) (dto.SetOrderStatusResponse, error)
+	GetOrderStatus(ctx context.Context, req dto.GetOrderStatusRequest) (dto.GetOrderStatusResponse, error)
+	SetOrderSTN(ctx context.Context, req dto.SetOrderSTNRequest) (dto.SetOrderSTNResponse, error)
+	SetOrderPromo(ctx context.Context, req dto.SetOrderPromoRequest) (dto.SetOrderPromoResponse, error)
+	RemoveOrderPromo(ctx context.Context, req dto.RemoveOrderPromoRequest) (dto.RemoveOrderPromoResponse, error)
+	DeleteOrder(ctx context.Context, req dto.DeleteOrderRequest) (dto.DeleteOrderResponse, error)
+
+	GetAllOrders(ctx context.Context, req dto.GetAllOrdersRequest) (dto.GetAllOrdersResponse, error)
+	GetAllOrdersByStatus(ctx context.Context, req dto.GetAllOrdersByStatusRequest) (dto.GetAllOrdersByStatusResponse, error)
+	GetUserOrders(ctx context.Context, req dto.GetUserOrdersRequest) (dto.GetUserOrdersResponse, error)
+	GetUserOrdersByStatus(ctx context.Context, req dto.GetUserOrdersByStatusRequest) (dto.GetUserOrdersByStatusResponse, error)
+	GetDateOrders(ctx context.Context, req dto.GetDateOrdersRequest) (dto.GetDateOrdersResponse, error)
+	GetDateOrdersByStatus(ctx context.Context, req dto.GetDateOrdersByStatusRequest) (dto.GetDateOrdersByStatusResponse, error)
+
+	GetAllPromos(ctx context.Context, req dto.GetAllPromosRequest) (dto.GetAllPromosResponse, error)
+	GetPromoByOrder(ctx context.Context, req dto.GetPromoByOrderRequest) (dto.GetPromoByOrderResponse, error)
+	GetUserPromos(ctx context.Context, req dto.GetUserPromosRequest) (dto.GetUserPromosResponse, error)
+
+	//CreateEmptyOrder(ctx context.Context, req dto.CreateEmptyOrderRequest) (dto.CreateEmptyOrderResponse, error)
+	//CheckOpenOrder(ctx context.Context, req dto.CheckOpenOrderRequest) (dto.CheckOpenOrderResponse, error)
+}
+
+type UseCaseRepo struct {
+	repo Repository
+}
+
+func New(r Repository) UseCaseRepo {
+	return UseCaseRepo{repo: r}
+}
+
+/*
+func (u UseCaseRepo) CreateEmptyOrder(ctx context.Context, req dto.CreateEmptyOrderRequest) (dto.CreateEmptyOrderResponse, error) {
+
+	orderID, err := u.repo.CreateEmptyOrder(ctx, req.UserID)
+	if err != nil {
+		return dto.CreateEmptyOrderResponse{}, err
+	}
+
+	return dto.CreateEmptyOrderResponse{OrderID: orderID}, nil
+}
+*/
+/*
+func (u UseCaseRepo) CheckOpenOrder(ctx context.Context, req dto.CheckOpenOrderRequest) (dto.CheckOpenOrderResponse, error) {
+
+	orderID, err := u.repo.CheckOpenOrder(ctx, req.UserID)
+	if err != nil {
+		return dto.CheckOpenOrderResponse{}, err
+	}
+
+	return dto.CheckOpenOrderResponse{OrderID: orderID}, nil
+}
+*/
+
+func (u UseCaseRepo) AddItem(ctx context.Context, req dto.AddItemRequest) (dto.AddItemResponse, error) {
+
+	err := u.repo.AddItem(ctx, req.Item, req.UserID)
+	if err != nil {
+		return dto.AddItemResponse{}, err
+	}
+
+	return dto.AddItemResponse{}, nil
+}
+
+func (u UseCaseRepo) IncreaseQuantity(ctx context.Context, req dto.IncreaseQuantityRequest) (dto.IncreaseQuantityResponse, error) {
+
+	err := u.repo.IncreaseQuantity(ctx, req.ItemID, req.OrderID)
+	if err != nil {
+		return dto.IncreaseQuantityResponse{}, err
+	}
+
+	return dto.IncreaseQuantityResponse{}, nil
+}
+
+func (u UseCaseRepo) DecreaseQuantity(ctx context.Context, req dto.DecreaseQuantityRequest) (dto.DecreaseQuantityResponse, error) {
+
+	err := u.repo.DecreaseQuantity(ctx, req.ItemID, req.OrderID)
+	if err != nil {
+		return dto.DecreaseQuantityResponse{}, err
+	}
+
+	return dto.DecreaseQuantityResponse{}, nil
+}
+
+func (u UseCaseRepo) GetOrderItems(ctx context.Context, req dto.GetOrderItemsRequest) (dto.GetOrderItemsResponse, error) {
+
+	items, err := u.repo.GetOrderItems(ctx, req.OrderID)
+	if err != nil {
+		return dto.GetOrderItemsResponse{}, err
+	}
+
+	return dto.GetOrderItemsResponse{Items: items}, nil
+}
+
+func (u UseCaseRepo) RemoveItem(ctx context.Context, req dto.RemoveItemRequest) (dto.RemoveItemResponse, error) {
+
+	err := u.repo.RemoveItem(ctx, req.ItemID, req.OrderID)
+	if err != nil {
+		return dto.RemoveItemResponse{}, err
+	}
+
+	return dto.RemoveItemResponse{}, nil
+}
+
+func (u UseCaseRepo) CreatePromoCode(ctx context.Context, req dto.CreatePromoCodeRequest) (dto.CreatePromoCodeResponse, error) {
+
+	err := u.repo.CreatePromoCode(ctx, req.Promo, req.UserID)
+	if err != nil {
+		return dto.CreatePromoCodeResponse{}, err
+	}
+
+	return dto.CreatePromoCodeResponse{}, nil
+}
+
+func (u UseCaseRepo) DeletePromoCode(ctx context.Context, req dto.DeletePromoCodeRequest) (dto.DeletePromoCodeResponse, error) {
+
+	err := u.repo.DeletePromoCode(ctx, req.PromoID)
+	if err != nil {
+		return dto.DeletePromoCodeResponse{}, err
+	}
+
+	return dto.DeletePromoCodeResponse{}, nil
+}
+
+func (u UseCaseRepo) SetOrderStatus(ctx context.Context, req dto.SetOrderStatusRequest) (dto.SetOrderStatusResponse, error) {
+
+	err := u.repo.SetOrderStatus(ctx, req.Status, req.OrderID)
+	if err != nil {
+		return dto.SetOrderStatusResponse{}, err
+	}
+
+	return dto.SetOrderStatusResponse{}, nil
+}
+
+func (u UseCaseRepo) GetOrderStatus(ctx context.Context, req dto.GetOrderStatusRequest) (dto.GetOrderStatusResponse, error) {
+
+	status, err := u.repo.GetOrderStatus(ctx, req.OrderID)
+	if err != nil {
+		return dto.GetOrderStatusResponse{}, err
+	}
+
+	return dto.GetOrderStatusResponse{Status: status}, nil
+}
+
+func (u UseCaseRepo) SetOrderSTN(ctx context.Context, req dto.SetOrderSTNRequest) (dto.SetOrderSTNResponse, error) {
+
+	err := u.repo.SetOrderSTN(ctx, req.STN, req.OrderID)
+	if err != nil {
+		return dto.SetOrderSTNResponse{}, err
+	}
+
+	return dto.SetOrderSTNResponse{}, nil
+}
+
+func (u UseCaseRepo) SetOrderPromo(ctx context.Context, req dto.SetOrderPromoRequest) (dto.SetOrderPromoResponse, error) {
+
+	err := u.repo.SetOrderPromo(ctx, req.OrderID, req.PromoCode, req.UserID)
+	if err != nil {
+		return dto.SetOrderPromoResponse{}, err
+	}
+
+	return dto.SetOrderPromoResponse{}, nil
+}
+
+func (u UseCaseRepo) RemoveOrderPromo(ctx context.Context, req dto.RemoveOrderPromoRequest) (dto.RemoveOrderPromoResponse, error) {
+
+	err := u.repo.RemoveOrderPromo(ctx, req.OrderID)
+	if err != nil {
+		return dto.RemoveOrderPromoResponse{}, err
+	}
+
+	return dto.RemoveOrderPromoResponse{}, nil
+}
+
+func (u UseCaseRepo) DeleteOrder(ctx context.Context, req dto.DeleteOrderRequest) (dto.DeleteOrderResponse, error) {
+
+	err := u.repo.DeleteOrder(ctx, req.OrderID)
+	if err != nil {
+		return dto.DeleteOrderResponse{}, err
+	}
+
+	return dto.DeleteOrderResponse{}, nil
+}
+
+func (u UseCaseRepo) GetAllOrders(ctx context.Context, req dto.GetAllOrdersRequest) (dto.GetAllOrdersResponse, error) {
+
+	orders, err := u.repo.GetAllOrders(ctx)
+	if err != nil {
+		return dto.GetAllOrdersResponse{}, err
+	}
+
+	return dto.GetAllOrdersResponse{Orders: orders}, nil
+}
+
+func (u UseCaseRepo) GetAllOrdersByStatus(ctx context.Context, req dto.GetAllOrdersByStatusRequest) (dto.GetAllOrdersByStatusResponse, error) {
+
+	orders, err := u.repo.GetAllOrdersByStatus(ctx, req.Status)
+	if err != nil {
+		return dto.GetAllOrdersByStatusResponse{}, err
+	}
+
+	return dto.GetAllOrdersByStatusResponse{Orders: orders}, nil
+}
+
+func (u UseCaseRepo) GetUserOrders(ctx context.Context, req dto.GetUserOrdersRequest) (dto.GetUserOrdersResponse, error) {
+
+	orders, err := u.repo.GetUserOrders(ctx, req.UserID)
+	if err != nil {
+		return dto.GetUserOrdersResponse{}, err
+	}
+
+	return dto.GetUserOrdersResponse{Orders: orders}, nil
+}
+
+func (u UseCaseRepo) GetUserOrdersByStatus(ctx context.Context, req dto.GetUserOrdersByStatusRequest) (dto.GetUserOrdersByStatusResponse, error) {
+
+	orders, err := u.repo.GetUserOrdersByStatus(ctx, req.UserID, req.Status)
+	if err != nil {
+		return dto.GetUserOrdersByStatusResponse{}, err
+	}
+
+	return dto.GetUserOrdersByStatusResponse{Orders: orders}, nil
+}
+
+func (u UseCaseRepo) GetDateOrders(ctx context.Context, req dto.GetDateOrdersRequest) (dto.GetDateOrdersResponse, error) {
+
+	orders, err := u.repo.GetDateOrders(ctx, req.Date)
+	if err != nil {
+		return dto.GetDateOrdersResponse{}, err
+	}
+
+	return dto.GetDateOrdersResponse{Orders: orders}, nil
+}
+
+func (u UseCaseRepo) GetDateOrdersByStatus(ctx context.Context, req dto.GetDateOrdersByStatusRequest) (dto.GetDateOrdersByStatusResponse, error) {
+
+	orders, err := u.repo.GetDateOrdersByStatus(ctx, req.Date, req.Status)
+	if err != nil {
+		return dto.GetDateOrdersByStatusResponse{}, err
+	}
+
+	return dto.GetDateOrdersByStatusResponse{Orders: orders}, nil
+}
+
+func (u UseCaseRepo) GetAllPromos(ctx context.Context, req dto.GetAllPromosRequest) (dto.GetAllPromosResponse, error) {
+
+	promos, err := u.repo.GetAllPromos(ctx)
+	if err != nil {
+		return dto.GetAllPromosResponse{}, err
+	}
+
+	return dto.GetAllPromosResponse{Promos: promos}, nil
+}
+
+func (u UseCaseRepo) GetUserPromos(ctx context.Context, req dto.GetUserPromosRequest) (dto.GetUserPromosResponse, error) {
+
+	promos, err := u.repo.GetUserPromos(ctx, req.UserID)
+	if err != nil {
+		return dto.GetUserPromosResponse{}, err
+	}
+
+	return dto.GetUserPromosResponse{Promos: promos}, nil
+}
+
+func (u UseCaseRepo) GetPromoByOrder(ctx context.Context, req dto.GetPromoByOrderRequest) (dto.GetPromoByOrderResponse, error) {
+
+	promo, err := u.repo.GetPromoByOrder(ctx, req.OrderID)
+	if err != nil {
+		return dto.GetPromoByOrderResponse{}, err
+	}
+
+	return dto.GetPromoByOrderResponse{Promo: promo}, nil
+}
